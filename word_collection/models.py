1e5fdc4feb46773c2e01efb687442bcda8e8bd68
ea.almikeev <ea.almikeev@tensor.ru>
2025-04-03 16:26:35 +0300

    parts of speech added

--- word_collection/models.py
+++ word_collection/models.py
@@ -17,14 +17,27 @@ class Genus(models.IntegerChoices):
     NEUTRAL = 3, 'Neutral'
     PLURAL = 4, 'Plural'
 
+class PartOfSpeech(models.IntegerChoices):
+    NOUN = 1, 'Noun'
+    VERB = 2, 'Verb'
+    ADJECTIVE = 3, 'Adjective'
+    PRONOUN = 4, 'Pronoun'
+    ADVERB = 5, 'Adverb'
+    PREPOSITION = 6, 'Preposition'
+    NUMBER = 7, 'Number'
+    ARTICLE = 8, 'Article'
+    CONJUNCTION = 9, 'Conjunction'
+    INTERJECTION = 10, 'Interjection'
+
 class Word(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="words", db_index=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='words', db_index=True)
     created_date_time = models.DateTimeField(default=now)
     definition = models.TextField(max_length=100)
     translation = models.TextField(max_length=100)
     example = models.TextField(max_length=200)
     genus_id = models.IntegerField(choices=Genus.choices, null=True)
-    categories = models.ManyToManyField(Category, related_name="words")
+    categories = models.ManyToManyField(Category, related_name='words')
+    part_of_speech = models.IntegerField(choices=PartOfSpeech, null=True)
     usage_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
