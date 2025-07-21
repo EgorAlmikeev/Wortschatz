07c09c30af1cd20082cb0673173ac85edf197b7c
ea.almikeev <ea.almikeev@tensor.ru>
2025-07-21 18:11:42 +0300

    word-collection admin update

--- word_collection/admin.py
+++ word_collection/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Word, Category
 
-# Register your models here.
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ['definition', 'owner']
+    list_filter = ['genus_id', 'categories']
+    search_fields = ['owner__username']
+    date_hierarchy = 'created_date_time'
+    autocomplete_fields = ['owner', 'categories']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner']
+    search_fields = ['owner__username']
