ee25948173db303450855749d376820a03245ed1
ea.almikeev <ea.almikeev@tensor.ru>
2025-08-03 16:16:50 +0300

    word definition filter add

--- word_collection/filters.py
+++ word_collection/filters.py
@@ -4,6 +4,7 @@ from .models import Word, Category
 class WordFilter(django_filters.FilterSet):
     part_of_speech_id = django_filters.NumberFilter()
     created_date_time = django_filters.DateFromToRangeFilter()
+    definition = django_filters.CharFilter(lookup_expr='icontains')
     categories = django_filters.ModelMultipleChoiceFilter(
         field_name='categories',
         queryset=Category.objects.all()
