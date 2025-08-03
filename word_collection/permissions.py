8868331f8ae00e81b77761dce940d89bf78db836
ea.almikeev <ea.almikeev@tensor.ru>
2025-08-03 16:17:01 +0300

    permissions update

--- word_collection/permissions.py
+++ word_collection/permissions.py
@@ -1,8 +1,8 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from .utils import get_superuser_id
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'HEAD']:
+        if request.method in SAFE_METHODS:
             return True
         return request.user == obj.owner or request.user.id == get_superuser_id()
