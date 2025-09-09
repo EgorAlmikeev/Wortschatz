3a4ce25423292dc9234c6d6e0c388e67a34cdd00
ea.almikeev <ea.almikeev@tensor.ru>
2025-09-09 13:37:43 +0300

    tests add

---
+++ word_collection/mockups.py
@@ -0,0 +1,14 @@
+class Mockups:
+
+    category_mockup = {
+            'name': 'test name',
+        }
+
+    word_mockup = {
+            'definition': 'test definition',
+            'translations': ['test translation'],
+            'examples': ['test example'],
+            'other_forms': ['test other form'],
+            'part_of_speech_id': 1,
+            'genus_id': 1
+        }
--- word_collection/tests.py
+++ word_collection/tests.py
@@ -1,32 +1,40 @@
 import pytest
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+
 from word_collection.models import Word, Category
+from word_collection.mockups import Mockups
 
-@pytest.mark.django_db
-def test_create_word():
-    user = User.objects.create_user(username='testuser', password='12345')
-    client = APIClient()
-    client.force_authenticate(user=user)
+class TestWordCollection:
 
-    data = {
-        'definition': 'test definition',
-        'translations': ['test translation'],
-        'examples': ['test example'],
-        'other_forms': ['test other form'],
-        'part_of_speech_id': 1,
-        'genus_id': 1
-    }
+    def setup_method(self, method):
+        print()
+        print(f"=== creating user for a '{method.__name__}' test ===")
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
 
-    response = client.post('/api/words/', data, format='json')
+    @pytest.mark.django_db
+    def test_create_word(self):
+        data = Mockups.word_mockup
+        response = self.client.post('/api/words/', data, format='json')
 
-    if response.status_code != 201:
-        print(response.data)
+        assert response.status_code == 201
+        assert response.data['definition'] == data['definition']
+        assert response.data['translations'] == data['translations']
+        assert response.data['examples'] == data['examples']
+        assert response.data['other_forms'] == data['other_forms']
+        assert response.data['part_of_speech_id'] == data['part_of_speech_id']
+        assert response.data['genus_id'] == data['genus_id']
+    
+    @pytest.mark.django_db
+    def test_create_category(self):
+        data = Mockups.category_mockup
 
-    assert response.status_code == 201
-    assert response.data['definition'] == data['definition']
-    assert response.data['translations'] == data['translations']
-    assert response.data['examples'] == data['examples']
-    assert response.data['other_forms'] == data['other_forms']
-    assert response.data['part_of_speech_id'] == data['part_of_speech_id']
-    assert response.data['genus_id'] == data['genus_id']
+        response = self.client.post('/api/categories/', data, format='json')
+
+        if response.status_code != 201:
+            print(response.data)
+        
+        assert response.status_code == 201
+        assert response.data['name'] == data['name']
